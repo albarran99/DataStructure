5a7dd1a1b5566b1b8b48655470de1665e70c61c7
albarran99 <78142240+albarran99@users.noreply.github.com>
2021-03-23 18:10:06 +0100

    update Data Structure

--- src/main/java/org/iesfm/datastructure/sets/ListWihtSet.java
+++ src/main/java/org/iesfm/datastructure/sets/ListWihtSet.java
@@ -1,10 +1,35 @@
 package org.iesfm.datastructure.sets;
 
-import java.util.Set;
+import java.util.*;
 
 public class ListWihtSet {
     public boolean checkNames(Set<String> names, String name) {
 
         return names.contains(name);
     }
+
+    public Set<String> insertNames(Set<String> names, String name) {
+        names.add(name);
+
+        return names;
+    }
+
+    public List<String> toList(Set<String> names) {
+        List<String> list = new ArrayList<>(names);
+
+        return list;
+    }
+
+    public TreeSet<String> toListReverse(List<String> names) {
+        Comparator<String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        };
+        TreeSet<String> newSet = new TreeSet<>(comparator);
+        newSet.addAll(names);
+        return newSet;
+    }
+
 }
--- src/test/java/org/iesfm/datastructure/sets/SetTest.java
+++ src/test/java/org/iesfm/datastructure/sets/SetTest.java
@@ -4,8 +4,7 @@ package org.iesfm.datastructure.sets;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class SetTest {
     @Test
@@ -17,8 +16,139 @@ public class SetTest {
         names.add("Carlos");
         names.add("Ana");
 
-        boolean expected = listWihtSet.checkNames(names,"Jose");
+        boolean expected = listWihtSet.checkNames(names, "Jose");
 
         Assert.assertTrue(expected);
     }
+
+    @Test
+    public void insertNameTest() {
+        ListWihtSet listWihtSet = new ListWihtSet();
+        Set<String> names = new HashSet<>();
+
+        names.add("Jose");
+        names.add("Carlos");
+
+        listWihtSet.insertNames(names, "Ana");
+
+        Set<String> expected = new HashSet<>();
+
+        expected.add("Jose");
+        expected.add("Carlos");
+        expected.add("Ana");
+
+
+        Assert.assertEquals(expected, names);
+
+    }
+
+    @Test
+    public void insertNameSameNameTest() {
+        ListWihtSet listWihtSet = new ListWihtSet();
+        Set<String> names = new HashSet<>();
+
+        names.add("Jose");
+
+
+        listWihtSet.insertNames(names, "Jose");
+
+        Set<String> expected = new HashSet<>();
+
+        expected.add("Jose");
+
+
+        Assert.assertEquals(expected, names);
+
+    }
+
+    @Test
+    public void toListTreeSetTest() {
+        ListWihtSet listWihtSet = new ListWihtSet();
+
+
+        Set<String> names = new TreeSet<>();
+
+        names.add("Charlyno");
+        names.add("Albarran");
+        names.add("Balvario");
+        names.add("Dedario");
+        names.add("Federico");
+        names.add("Eustaquio");
+
+        List<String> list = listWihtSet.toList(names);
+
+        List<String> expected = new ArrayList<>();
+
+        expected.add("Albarran");
+        expected.add("Balvario");
+        expected.add("Charlyno");
+        expected.add("Dedario");
+        expected.add("Eustaquio");
+        expected.add("Federico");
+
+
+        Assert.assertEquals(expected, list);
+
+    }
+
+    @Test
+    public void toListHashSetTest() {
+        ListWihtSet listWihtSet = new ListWihtSet();
+
+
+        Set<String> names = new HashSet<>();
+
+        names.add("Charlyno");
+        names.add("Albarran");
+        names.add("Balvario");
+        names.add("Dedario");
+        names.add("Federico");
+        names.add("Eustaquio");
+
+        List<String> list = listWihtSet.toList(names);
+
+        List<String> expected = new ArrayList<>();
+
+        expected.add("Albarran");
+        expected.add("Balvario");
+        expected.add("Charlyno");
+        expected.add("Dedario");
+        expected.add("Eustaquio");
+        expected.add("Federico");
+
+
+        Assert.assertEquals(expected, list);
+
+    }
+
+    @Test
+    public void toSetTest() {
+        ListWihtSet listWihtSet = new ListWihtSet();
+
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Charlyno");
+        names.add("Albarran");
+        names.add("Balvario");
+        names.add("Dedario");
+        names.add("Federico");
+        names.add("Eustaquio");
+
+        Set<String> list = listWihtSet.toListReverse(names);
+
+        TreeSet<String> expected = new TreeSet<>();
+
+        expected.add("Albarran");
+        expected.add("Balvario");
+        expected.add("Charlyno");
+        expected.add("Dedario");
+        expected.add("Eustaquio");
+        expected.add("Federico");
+
+
+        Assert.assertEquals(expected, list);
+
+    }
+
 }
